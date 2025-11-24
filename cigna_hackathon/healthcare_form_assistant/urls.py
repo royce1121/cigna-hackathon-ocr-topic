625e588e76269c733a0fa702889903568071c039
Royce Ivan <royce.i.fadriquela@accenture.com>
2025-11-25 06:57:02 +0800

    simulate OCR convertion

--- cigna_hackathon/healthcare_form_assistant/urls.py
+++ cigna_hackathon/healthcare_form_assistant/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import MemberView, GetMemberTranscations
+from .views import MemberView, GetMemberTranscations, OCRView
 
 urlpatterns = [
     path('member/', MemberView.as_view(), name='member-view'),
     path('transactions/<str:ph_id>/', GetMemberTranscations.as_view(), name='member-transactions'),
+    path('ocr/', OCRView.as_view(), name='ocr'),
 ]
--- cigna_hackathon/healthcare_form_assistant/views.py
+++ cigna_hackathon/healthcare_form_assistant/views.py
@@ -3,7 +3,11 @@ from django.http import HttpResponse
 from django.views import View
 from django.views.generic import DetailView, ListView
 from .models import Patient, MedicalTransactions
+from paddleocr import PaddleOCR
+import os
+import fitz
 
+ocr = PaddleOCR(lang='en')
 class MemberView(View):
     template_name = 'member/landing_page.html'
 
@@ -28,13 +32,48 @@ class GetMemberTranscations(ListView):
         return MedicalTransactions.objects.filter(
             patient=patient
         ).order_by('date')
-    # def get(self, request):
-    #     ph_id = request.GET.get("ph_id")
-    #     patient = Patient.objects.filter(ph_id=ph_id)
-    #     mt_query = None
-    #     if patient:
-    #         patient = patient.first()
-    #         mt_query = MedicalTransactions.objects.filter(
-    #             patient=patient
-    #         ).order_by('date')
-    #     return render(request, self.template_name, {'ph_id': ph_id, 'transactions': mt_query})
+
+
+class OCRView(View):
+    template_name = "member/ocr_reader.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+    def post(self, request):
+        selected_file = request.FILES.get("file")
+        text_result = []
+
+        if selected_file:
+            temp_dir = "temp_files"
+            file_path = os.path.join(temp_dir, selected_file.name)
+            os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+            with open(file_path, "wb+") as f:
+                for chunk in selected_file.chunks():
+                    f.write(chunk)
+            
+            # OCR Process
+            transform_pdf = fitz.open(file_path)
+            for page_number, page in enumerate(transform_pdf):
+                print('=============')
+                page_text = page.get_text("text").strip()
+                if page_text:
+                    print(page_text)
+                print('=======================')
+                pix = page.get_pixmap()
+                img_path = os.path.join(temp_dir, f"{selected_file.name}_page_{page_number}.png")
+                pix.save(img_path)
+                try:
+                    result = ocr.predict(img_path)
+                    print(result)
+                    print('111111111111111111111111111111')
+                    for page_result in result:
+                        rec_texts = page_result.get('rec_texts', [])
+                        text_result.extend(rec_texts)
+                except Exception as e:
+                    text_result.append(f"Error processing page {page_number}: {e}")
+        print('================')
+        print(text_result)
+        print('=====================')
+        return render(request, self.template_name, {"text_result": text_result})
